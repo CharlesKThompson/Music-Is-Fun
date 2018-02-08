f746bdd3492b324f2d312f510a01b464d21d77fa
Charles <chuckness32@hotmail.com>
2018-02-07 21:59:02 -0700

    second commit

--- itunes-controller.js
+++ itunes-controller.js
@@ -19,20 +19,26 @@ function ItunesController() {
       var song = songs[i];
       template += `
     <li>
-      <div class="col-sm-4">
       <h3> ${song.title}</h3>
       <img src= "${song.albumArt}" alt="cover">
       <p> ${song.artist}</p>
       <p> ${song.collection}</p>
-      <p> ${song.price}</p>
+      <p> ${"$" + song.price}</p>
       <audio controls="controls">
       <source src="${song.preview}">
       </audio>
-      </div>
     </li>
     `
+  //   document.addEventListener('play', function(e){
+  //     var audios = document.getElementsById('song.price');
+  //     for(var i = 0, len = audios.length; i < len;i++){
+  //         if(audios[i] != e.target){
+  //             audios[i].pause();
+  //         }
+  //     }
+  // }, true);
     getMusic.innerHTML= template
-
+    
     }
 
 
